624ecb04061a1ec4b52f1d50152f0af057937ef2
ikadimi <ilkadimi@outlook.com>
2024-09-25 17:05:01 +0200

    adding email validation

--- models/user.model.js
+++ models/user.model.js
@@ -25,6 +25,10 @@ const userSchema = new Schema({
     required: [true, 'Password is required'],
     minlength: [6, 'Password must be at least 6 characters long'],
   },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
 }, {
   timestamps: true, // Add createdAt and updatedAt fields
 });
--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const amqp = require('amqplib');
 const jwt = require('jsonwebtoken');
-const secret = 'secret';
 const User = require('../models/user.model'); // Adjust path as necessary
 const createResponse = require('../models/response.model');
+
 
 function authenticateToken(req, res, next) {
     const token = req.cookies.jwt; // Read token from cookies
     if (token == null) return res.sendStatus(401); // No token
   
-    jwt.verify(token, secret, (err, user) => {
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
       if (err) return res.sendStatus(403); // Invalid token
       req.user = user;
       next();
     });
+}
+
+async function publishEmail(mail) {
+    try {
+      const connection = await amqp.connect(process.env.RABBITMQ_URL);
+      const channel = await connection.createChannel();
+      const queue = 'email_queue';
+  
+      await channel.assertQueue(queue, { durable: true });
+      channel.sendToQueue(queue, Buffer.from(JSON.stringify(mail)), { persistent: true });
+  
+      console.log('Email request sent to queue');
+      await channel.close();
+      await connection.close();
+    } catch (error) {
+      console.error('Failed to send email request to queue:', error);
+    }
 }
 
 router.get('/me', authenticateToken, (req, res) => {
@@ -21,6 +39,15 @@ router.get('/me', authenticateToken, (req, res) => {
         id: req.user.id,
         username: req.user.username,
     });
+});
+
+router.get('/user/:id', async (req, res) => {
+    const userId = req.params.id;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json({ username: user.username, email: user.email });
 });
 
 router.post('/login', async (req, res) => {
@@ -33,6 +60,10 @@ router.post('/login', async (req, res) => {
             return res.status(401).json({ message: 'Invalid email or password' });
         }
 
+        if (!user.isVerified) {
+            return res.status(400).json({ message: 'Please verify your email before logging in.' });
+        }
+
         // Check if the password matches
         const isMatch = await user.comparePassword(password);
         if (!isMatch) {
@@ -40,7 +71,7 @@ router.post('/login', async (req, res) => {
         }
 
         // Generate JWT token
-        const token = jwt.sign({ userId: user._id, username: user.username }, secret);
+        const token = jwt.sign({ userId: user._id, username: user.username }, process.env.JWT_SECRET);
         res.cookie('jwt', token, { httpOnly: true });
 
         res.status(200).json(createResponse(true, 'Login successful'));
@@ -49,15 +80,52 @@ router.post('/login', async (req, res) => {
     }
 });
 
+router.post('/verify-email', async (req, res) => {
+    const { token } = req.body;
+    
+    try {
+      // Verify the token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      
+      // Find the user by email
+      const user = await User.findOne({ email: decoded.email });
+      if (!user) {
+        return res.status(400).json({ message: 'Invalid token or user does not exist' });
+      }
+  
+      // Update the user's verification status
+      user.isVerified = true;
+      await user.save();
+  
+      res.status(200).json({ message: 'Email verified successfully. You can now log in.' });
+      
+    } catch (err) {
+      res.status(400).json({ message: 'Invalid or expired token' });
+    }
+});
+  
+
 router.post('/register', async (req, res) => {
     const { username, email, password } = req.body;
 
     try {
         // Create a new user
-        const user = new User({ username, email, password });
-        await user.save();
+        const newUser = new User({ username, email, password, isVerified: false });
+        await newUser.save();
 
-        res.status(201).json(createResponse(true, 'Registration successful'));
+        const token = jwt.sign({ email: newUser.email }, process.env.JWT_SECRET, { expiresIn: '1d' });
+
+        const verificationLink = `http://localhost:4200/login?verification_token=${token}`;
+
+        // Send the verification email
+        await publishEmail({
+            to: newUser.email,
+            subject: 'ECOMMERCE: Verify Your Email',
+            text: `Click the link to verify your email: ${verificationLink}`,
+            html: `<p>Click <a href="${verificationLink}">here</a> to verify your email.</p>`
+        });
+
+        res.status(201).json(createResponse(true, 'Registration successful. Please check your email to verify your account.'));
     } catch (error) {
         console.error('Registration error:', {...error});
         res.status(500).json({ message: error.message });
